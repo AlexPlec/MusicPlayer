445a517b03e844c6015bd8d8963a5bc959157bde
AlexPlec <acchtc724@gmail.com>
2025-03-12 13:43:22 +0200

    added keys, tray

--- public/renderer.js
+++ public/renderer.js
@@ -4,11 +4,11 @@ const audioPlayer = document.getElementById('audioPlayer');
 const path = require('path'); // Add this line
 
 let musicFiles = [];
+let currentTrackIndex = -1;
 
 // Request the list of music files from the main process
 ipcRenderer.invoke('get-music-files').then(files => {
   musicFiles = files;
-  console.log(musicFiles);
   renderPlaylist();
 });
 
@@ -29,3 +29,39 @@ function renderPlaylist() {
     playlist.appendChild(songItem);
   });
 }
+
+function playTrack(index) {
+    if (index >= 0 && index < musicFiles.length) {
+      const file = musicFiles[index];
+      audioPlayer.src = file;
+      audioPlayer.play();
+      currentTrackIndex = index; // Update the current track index
+    }
+  }
+
+document.addEventListener('keydown', (event) => {
+    if (event.code === 'Space') {
+      event.preventDefault(); // Prevent default scrolling behavior
+      if (audioPlayer.paused) {
+        audioPlayer.play();
+      } else {
+        audioPlayer.pause();
+      }
+    }
+
+    // Handle left arrow key to play the previous track
+  if (event.code === 'ArrowLeft') {
+    event.preventDefault();
+    if (currentTrackIndex > 0) {
+      playTrack(currentTrackIndex - 1); // Play the previous track
+    }
+  }
+
+  // Handle right arrow key to play the next track
+  if (event.code === 'ArrowRight') {
+    event.preventDefault();
+    if (currentTrackIndex < musicFiles.length - 1) {
+      playTrack(currentTrackIndex + 1); // Play the next track
+    }
+  }
+});
